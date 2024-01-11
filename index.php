97f411abfc5d7df5f3050f6b0beae5a8efb0c342
xarop <ajl@xarop.com>
2024-01-11 17:53:22 +0100

    cv

--- index.php
+++ index.php
@@ -480,6 +480,12 @@ function displayAllPosts($posts_count = 5, $excerpt_length = 75) {
       <ul id="menu-print" class="social-links-menu no-print">
 
         <li>
+          <a href="https://xarop-com.translate.goog/cv/?_x_tr_sl=es&_x_tr_tl=en&_x_tr_hl=ca&_x_tr_pto=wapp" target="_blank" title="Translate"><span>Translate</span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path fill="currentColor" d="M27.85 29H30l-6-15h-2.35l-6 15h2.15l1.6-4h6.85zm-7.65-6l2.62-6.56L25.45 23zM18 7V5h-7V2H9v3H2v2h10.74a14.71 14.71 0 0 1-3.19 6.18A13.5 13.5 0 0 1 7.26 9h-2.1a16.47 16.47 0 0 0 3 5.58A16.84 16.84 0 0 1 3 18l.75 1.86A18.47 18.47 0 0 0 9.53 16a16.92 16.92 0 0 0 5.76 3.84L16 18a14.48 14.48 0 0 1-5.12-3.37A17.64 17.64 0 0 0 14.8 7z"/></svg>
+          </a>
+        </li>
+        
+        <li>
           <a href="ajlcv.pdf" target="_blank" title="Descargar PDF"><span>Descargar PDF</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
               <path fill="currentColor"
